ed17ac6ad29aecf4fa1e1161980e4803aec1af76
jeerapun <jeerapu.m@gmail.com>
2013-03-10 22:16:11 +0700

    add new user jeerapun.m

--- src/main/java/com/tmn/spring/account/service/impl/UserRepository.java
+++ src/main/java/com/tmn/spring/account/service/impl/UserRepository.java
@@ -20,8 +20,10 @@ public class UserRepository {
         repository = new HashMap<String, User>();
         User firstUser = new User("roofimon", "password");
         User secondUser = new User("boyone", "password");
+        User thirdUser = new User("jeerapun.m", "1234");
         repository.put(firstUser.getUsername(), firstUser);
         repository.put(secondUser.getUsername(), secondUser);
+        repository.put(thirdUser.getUsername(), thirdUser);
     }
     
     public User getUserByUsername(String username){
